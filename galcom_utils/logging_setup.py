b6f3f0e5c4ccfafc4e6f6c0ddc922e0bda8e2543
Kevin Horan <khoran@galcom.org>
2026-05-01 10:23:03 -0400

    check if loki is reachable and dont use it if not

--- galcom_utils/logging_setup.py
+++ galcom_utils/logging_setup.py
@@ -1,10 +1,25 @@
 import logging
 import socket
 from multiprocessing import Queue
+from urllib.error import HTTPError, URLError
+from urllib.request import Request, urlopen
 
 import logging_loki
 
 _loki_tags = None
+LOKI_URL = "http://192.168.0.192:3100/loki/api/v1/push"
+
+
+def _is_loki_url_reachable(url, timeout=2):
+    request = Request(url, method="GET")
+    try:
+        with urlopen(request, timeout=timeout):
+            return True
+    except HTTPError:
+        # Non-2xx still proves the endpoint is reachable.
+        return True
+    except (URLError, TimeoutError, OSError):
+        return False
 
 
 def get_and_config_logger(name):
@@ -38,9 +53,13 @@ def add_loki_logger(name, logger, formatter):
         "bridge_id": "unset",
     }
 
+    if not _is_loki_url_reachable(LOKI_URL):
+        print("not adding loki logger since endpoint is not reachable")
+        return
+
     loki_handler = logging_loki.LokiQueueHandler(
         Queue(-1),
-        url="http://192.168.0.192:3100/loki/api/v1/push",
+        url=LOKI_URL,
         tags=_loki_tags,
         version="1",
     )
